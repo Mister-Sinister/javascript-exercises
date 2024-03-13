a543a55beed2f0dea038387556308cb19818efe3
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-03-13 08:25:56 +0100

    feat: Implement solution for repeatString.js
    - Write function that repeats a string a given number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        let repeatedString = "";
+        while (num > 0) {
+            repeatedString += string;
+            num--;
+        }
+        return repeatedString    
+    }
 };
 
 // Do not edit below this line
