f0b5d06b05f97ebddccf6e12f7486629b0e61cfc
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-03-13 08:31:28 +0100

    feat: Implement 04_removeFromArray
    - Write function that takes and array and other args and removes other args from that array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    newArray = [];
+    for (let toRemove of args) {
+        for (let i in array) {
+            // if (array[i] === toRemove) {
+            //     array.splice(i,1);
 
+            // }
+            if (array[i] !== toRemove) {
+                newArray.push(array[i]);
+            }
+        }
+    }
+    return newArray
+    // return array;
 };
 
+
+removeFromArray([1, 2, 2, 3], 2);
 // Do not edit below this line
 module.exports = removeFromArray;
