0ef91722bfec7296b7213a0ab46bdf3578f04080
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-08-30 22:55:19 +0100

    feat: write function that returns fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  if (typeof member !== "number") {
+    member = parseInt(member);
+  }
+  if (member === 0) return 0;
+  if (member < 0) return "OOPS";
 
+  sequence = [1, 1];
+  for (let i = 2; i <= member; i++) {
+    let next_term = sequence[i - 1] + sequence[i - 2];
+    sequence.push(next_term);
+  }
+
+  return sequence[member - 1];
 };
 
 // Do not edit below this line
