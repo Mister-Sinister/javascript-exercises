d06f4a61966ea9990c011c2b71d8c897c97a7db2
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-08-30 16:25:06 +0100

    feat - Write a function to identify palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase();
 
+  let char_array = string
+    .split("")
+    .filter((char) => "abcdefghijklmnopqrstuvwxy0123456789".includes(char));
+
+  let cleanedString = char_array.join();
+  let reversedString = char_array.reverse().join();
+
+  if (reversedString === cleanedString) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
