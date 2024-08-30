1fdf680e6049f32873d997694287db81ea1330a7
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2024-08-30 11:03:24 +0100

    create math functions and test them using jest

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let ans = arr.reduce((total, current) => total + current, 0);
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, current) => {
+    return total * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let ans = 1;
+  while (a > 0) {
+    ans *= a;
+    a -= 1;
+  }
+  return ans;
 };
 
 // Do not edit below this line
-module.exports = {
+export default {
   add,
   subtract,
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
